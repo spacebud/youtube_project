04fac863508ea8b594d253d8b4b3f3b731a2c36c
stefanocassio <stefano.cassiobkk@gmail.com>
2016-01-24 21:16:20 +0700

    first commit

---
+++ JS/app.js
@@ -0,0 +1,31 @@
+
+function handleAPILoaded() {
+  $('#search-button').attr('disabled', false);
+}
+
+function search(){
+  gapi.client.setApiKey('AIzaSyCb6euLmg2fYjePaIF3h8JjfDG4WlktMeA');
+  gapi.client.load('youtube', 'v3', function() {
+    makeRequest();
+  });
+}
+
+
+function makeRequest() {
+  var q = $('#query').val();
+  var request = gapi.client.youtube.search.list({
+    q: q,
+    part: 'snippet'
+  });
+  request.execute(function(response) {
+    $('#results').empty()
+    var srchItems = response.result.items;
+    $.each(srchItems, function(index, item) {
+    vidTitle = item.snippet.title;
+    vidDescription = item.snippet.description;
+    vidThumburl =  item.snippet.thumbnails.default.url;
+    vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image Available." style="width:204px;height:128px"></pre>';
+    $('#results').append('<pre>' + vidTitle + vidThumbimg + vidDescription+'</pre>');
+   });
+ })
+}
